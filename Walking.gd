ebd83f1529f188a9a34db9c644ce5351f5374c02
Anderson kwan Odelish <andersonkwan8510@gmail.com>
2026-04-06 14:01:14 +0700

    nambah tombol

--- Walking.gd
+++ Walking.gd
@@ -12,4 +12,3 @@ func _physics_process(delta):
 	if direction != Vector2.ZERO:
 		var target_rotation = direction.angle() + deg_to_rad(180)
 		rotation = lerp_angle(rotation, target_rotation, 10 * delta)
-
