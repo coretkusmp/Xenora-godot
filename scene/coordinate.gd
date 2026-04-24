320fc0421983f2a7749aca3aedd3576bf3500dac
Anderson kwan Odelish <andersonkwan8510@gmail.com>
2026-04-24 17:21:40 +0700

    Update coordinate.gd

--- scene/coordinate.gd
+++ scene/coordinate.gd
@@ -1,5 +1,5 @@
 extends Label
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var ppxy = str(int($"../..".position.x),", ", int($"../..".position.y))
 	text = "player pos(x,y): " + ppxy
